ec762b8da3ebd412776e801e21b9ad635d81ea89
Maria Mayowa <129012596+mariamayowa@users.noreply.github.com>
2024-11-14 22:08:19 +0000

    Changed background colour and removed link.

---
+++ script.js
@@ -0,0 +1,23 @@
+function validateForm (event){
+    event.preventDefault()
+    const input=document.querySelectorAll("input[required]")
+    let allFilled=true
+    input.forEach(input =>{
+      if(!input.value.trim()){
+        allFilled = false;
+        input.style.border
+        ="1px solid red"; // Highlight empty fields
+      } else {
+        input.style.border=""; // Remove highlight if filled
+      }
+    });
+    // If all fields are filled, display submit message
+    if (allFilled) {
+    alert("Form submitted successfully!");
+      return true;
+    } else {
+      alert ("Please fill in all required fields.");
+      return false; 
+  }
+    console.log("Button pressed")
+  }
